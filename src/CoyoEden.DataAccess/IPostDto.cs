3191e85b5af6209bb04b7fecc70095600e0611bb
cocoayoyo <cocoayoyo@3a7b2ece-b80a-11de-9b33-83dc4fd2bb11>
2010-02-17 03:34:11 +0000

    ...

---
+++ src/CoyoEden.DataAccess/IPostDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoyoEden.DataAccess
+{
+    public interface IPostDto
+    {
+        Guid Id { get; set; }
+        string Title { get; set; }
+
+    }
+}
