b424efa9c0a59e42ba6cddede50305df01f71649
cocoayoyo <cocoayoyo@3a7b2ece-b80a-11de-9b33-83dc4fd2bb11>
2010-09-27 08:28:35 +0000

    ???????2-v0.9beta

--- src/focusImg/m/js/btslider.js
+++ src/focusImg/m/js/btslider.js
@@ -5,7 +5,10 @@
 		this._o=opts;
 		this.$l=$t;										/* 插件的dom布局 */
 		this.$items=$t.find(".lv_item");				/* 交互的元素 */
+		this.$oddItems=this.$items.filter(":odd");
+		this.$evenItems=this.$items.filter(":even");
 		this.imiddle=this.$items.length/2;				/* 中间元素索引 */
+		this.$hot=null;
 		//init
 		this._i(); 
 	};
@@ -28,7 +31,11 @@
 			//init events
 			this.$items.mouseenter(function(e){
 				_this.z($(this));
+			}).bind("evtReset",function(e){
+				_this.r($(this));
 			});
+			//init the zoomed items
+			this.$items.filter(".lv_init").trigger("mouseenter");
 		},
 		/**
 		 * zoom effect
@@ -37,6 +44,10 @@
 		z:function($t){
 			var _this=this;
 			if($t.hasClass("lv_zoom")) return;
+			if(this.$hot){
+				this.$hot.trigger("evtReset");
+			};
+			this.$hot=$t;
 			var i0=$t.index(),even=(i0%2==0),toLeft=(i0>=_this.imiddle);
 			this._zIn($t.addClass("lv_zoom"),even,toLeft);
 			this.$items.each(function(i,o){
@@ -58,50 +69,27 @@
 		 * @param {Object} toLeft
 		 */
 		_zIn:function($t,even,toLeft){
+			$t.stop(true, true);
+			$t.find("img").stop(true, true).animate({
+				width: this._o.w * 2 - this._o.gapH * 2,
+				height: this._o.h * 2 - this._o.gapV * 2
+			});	
 			if (even) {
-				if(toLeft){
-					$t.stop(true, true).animate({
-						width: this._o.w * 2,
-						height: this._o.h * 2,
+				if(toLeft){	
+					$t.animate({
 						left:'-='+this._o.w
 					});
-					$t.find("*").stop(true, true).animate({
-						width: this._o.w * 2 - this._o.gapH * 2,
-						height: this._o.h * 2 - this._o.gapV * 2
-					});					
-				}else{
-					$t.stop(true, true).animate({
-						width: this._o.w * 2,
-						height: this._o.h * 2
-					});
-					$t.find("*").stop(true, true).animate({
-						width: this._o.w * 2 - this._o.gapH * 2,
-						height: this._o.h * 2 - this._o.gapV * 2
-					});						
 				};
-
 			}else {
 				if(toLeft){
-					$t.stop(true, true).animate({
-						width: this._o.w * 2,
-						height: this._o.h * 2,
+					$t.animate({
 						top:0,
 						left:"-="+this._o.w
-					});
-					$t.find("*").stop(true, true).animate({
-						width: this._o.w * 2 - this._o.gapH * 2,
-						height: this._o.h * 2 - this._o.gapV * 2
-					});					
+					});										
 				}else{
-					$t.stop(true, true).animate({
-						width: this._o.w * 2,
-						height: this._o.h * 2,
+					$t.animate({
 						top:"-="+this._o.h
-					});
-					$t.find("*").stop(true, true).animate({
-						width: this._o.w * 2 - this._o.gapH * 2,
-						height: this._o.h * 2 - this._o.gapV * 2
-					});					
+					});				
 				};
 
 			};
@@ -111,25 +99,82 @@
 		 * @param {Object} $t
 		 */
 		r:function($t){
-			$t.stop(true,true).animate({width:this._o.w,height:this._o.h});
+			var _this=this;
+			var i0=$t.index(),even=(i0%2==0),toLeft=(i0>=this.imiddle),tempObj;
+			$t.removeClass("lv_zoom").find("img").stop(true,true).animate({width:this._o.w-this._o.gapH*2,height:this._o.h-this._o.gapV*2});
+			this.$items.stop(true,true);
+			if(even){
+				if(toLeft){
+					this.$evenItems.each(function(i,o){
+						i=(tempObj=$(o)).index();
+						if(i>=i0) return true;
+						tempObj.animate({left:"+="+_this._o.w});
+					});
+					this.$oddItems.each(function(i,o){
+						i=(tempObj=$(o)).index();
+						if(i>(i0+1)) return true;
+						tempObj.animate({left:"+="+_this._o.w*2});
+					});
+					$t.animate({left:"+="+this._o.w});	
+				}else{
+					this.$evenItems.each(function(i,o){
+						i=(tempObj=$(o)).index();
+						if(i<=i0) return true;
+						tempObj.animate({left:"-="+_this._o.w});
+					});
+					this.$oddItems.each(function(i,o){
+						i=(tempObj=$(o)).index();
+						if(i<=i0) return true;
+						tempObj.animate({left:"-="+_this._o.w*2});
+					});
+				};
+			}else{
+				if(toLeft){
+					this.$evenItems.each(function(i,o){
+						i=(tempObj=$(o)).index();
+						if(i>=i0) return true;
+						tempObj.animate({left:"+="+_this._o.w*2});
+					});
+					this.$oddItems.each(function(i,o){
+						i=(tempObj=$(o)).index();
+						if(i>=i0) return true;
+						tempObj.animate({left:"+="+_this._o.w});
+					});
+					$t.animate({left:"+="+_this._o.w,top:"+="+_this._o.h});					
+				}else{
+					this.$evenItems.each(function(i,o){
+						i=(tempObj=$(o)).index();
+						if(i<(i0-1)) return true;
+						tempObj.animate({left:"-="+_this._o.w*2});
+					});
+					this.$oddItems.each(function(i,o){
+						i=(tempObj=$(o)).index();
+						if(i<=i0) return true;
+						tempObj.animate({left:"-="+_this._o.w});
+					});
+					$t.animate({top:"+="+this._o.h});
+				};//if toLeft
+			};//end of if even
 		},
 		_goL:function($t,even,i,i0){
 			if(even&&i>(i0+1)) return;
 			if((!even)&&i>i0) return;
+			$t.stop(true,true);
 			if(i%2==0){
-				$t.stop(true,true).animate({left:"-="+this._o.w*(even?1:2)},600);
+				$t.animate({left:"-="+this._o.w*(even?1:2)},600);
 			}else{
-				$t.stop(true,true).animate({left:"-="+this._o.w*(even?2:1)},600);
+				$t.animate({left:"-="+this._o.w*(even?2:1)},600);
 			};
 
 		},
 		_goR:function($t,even,i,i0){
 			if(even&&i<i0) return;
 			if((!even)&&i<(i0-1)) return;
+			$t.stop(true,true);
 			if(i%2==0){
-				$t.stop(true,true).animate({left:"+="+this._o.w*(even?1:2)},600);
+				$t.animate({left:"+="+this._o.w*(even?1:2)},600);
 			}else{
-				$t.stop(true,true).animate({left:"+="+this._o.w*(even?2:1)},600);
+				$t.animate({left:"+="+this._o.w*(even?2:1)},600);
 			};	
 		}
 	};
@@ -145,8 +190,8 @@
     };
     // Public defaults.
     $.fn.btslide.defaults = {
-        w: 130,
-		h:175,
+        w: 140,
+		h:190,
 		gapV:5,				/* 图片垂直方向留白 */
 		gapH:5				/* 图片水平方向留白 */
     };
